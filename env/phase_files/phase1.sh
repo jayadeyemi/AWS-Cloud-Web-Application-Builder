399fca10be30ec704c1e939fd89c84a23091bf08
Adeyemi, Jimi <>
2024-12-18 09:21:08 -0500

    v3-line 30 mod

--- env/phase_files/phase1.sh
+++ env/phase_files/phase1.sh
@@ -23,12 +23,9 @@ if [[ $status -eq 0 ]]; then
     status=$?
 fi
 
-
-
 # Create a security group for the Cloud9 instance if not found
 if [[ $status -eq 0 ]]; then
-    execute_command "CLOUD9_SG_ID=\$(aws ec2 describe-instances --instance-ids \"$CLOUD9_INSTANCE_ID\" --query 'Reservations[0].Instances[0].SecurityGroups[0].GroupId' --output text)"
-    status=$?
+    CLOUD9_SG_ID=\$(aws ec2 describe-instances --instance-ids \"$CLOUD9_INSTANCE_ID\" --query 'Reservations[0].Instances[0].SecurityGroups[0].GroupId' --output text)
 fi
     if [[ -z "$CLOUD9_SG_ID" || "$CLOUD9_SG_ID" == "None" ]]; then
         # Assign attempt variables
