8688eaf5ff585ee8ed2ecac1ba610fc1c1d6a023
Adeyemi, Jimi <>
2024-12-19 04:59:04 -0500

    Main Function modified to automate deployment Currently Testing

--- env/config/functions.sh
+++ env/config/functions.sh
@@ -78,18 +78,23 @@ execute_command() {
 }
 
 ####################################################################################################
-# Phase Execution Wrapper Function
+# Main Worker Function
 ####################################################################################################
 
 prompt_phase() {
     local phase_num="$1"
     local phase_file="$2"
     local phase_name="$3"
+    local timed_out=0
 
     while true; do
         read -t 300 -r -p "Proceed to Phase ${phase_num} (${phase_name})? (y/n/[Press Enter to skip]): " cont
+        # Check if read timed out
+        if [[ $? -gt 0 ]]; then
+            timed_out=1
+        fi
         cont="${cont,,}"
-
+        # Check user input and proceed accordingly
         if [[ "$cont" == "y" ]]; then
             log "$EXECUTION_LOG" "Executing Phase ${phase_num} (${phase_name})..."
             source "$phase_file"
@@ -101,13 +106,23 @@ prompt_phase() {
         elif [[ "$cont" == "n" ]]; then
             log "$EXECUTION_LOG" "User chose to exit."
             exit 0
-        elif [[ -z "$cont" ]]; then
+        elif [[ -z "$cont" && $timed_out -eq 0 ]]; then
+            # Explicit Enter press (not timeout)
             log "$EXECUTION_LOG" "Skipping Phase ${phase_num}."
             break
+        elif [[ $timed_out -eq 1 ]]; then
+            # Handle timeout (automatic execution)
+            log "$EXECUTION_LOG" "Timeout reached. Automatically proceeding to Phase ${phase_num} (${phase_name})."
+            source "$phase_file"
+            if [[ $? -ne 0 ]]; then
+                log "$EXECUTION_LOG" "Phase ${phase_num} failed on timeout execution."
+                return 0
+            fi
+            break
         else
+            # Invalid input
             echo "Invalid input. Please enter 'y', 'n', or press Enter to skip."
         fi
-    done
 }
 
 ####################################################################################################
