91cb2cba52dae9daaa7909d4299232b2b5c59747
Adeyemi, Jimi <>
2024-12-21 00:51:08 -0500

    Finished. Ready Version 1

--- env/phase_files/phase3.sh
+++ env/phase_files/phase3.sh
@@ -28,6 +28,8 @@ fi
 # Create a launch template for ASG
 if [[ $status -eq 0 ]]; then
     execute_command "LAUNCH_TEMPLATE_ID=\$(aws ec2 create-launch-template --launch-template-name \"$LAUNCH_TEMPLATE_NAME\" --version-description \"Initial version\" --launch-template-data '{\"ImageId\":\"$SERVER_V2_IMAGE_ID\",\"InstanceType\":\"t2.micro\",\"KeyName\":\"$PRIVATE_KEY\",\"SecurityGroupIds\":[\"$ASG_SG_ID\"]}' --query 'LaunchTemplate.LaunchTemplateId' --output text)"
+    execute_command "LAUNCH_TEMPLATE_ID=\$(aws ec2 create-launch-template --launch-template-name \"$LAUNCH_TEMPLATE_NAME\" --version-description \"Initial version\" --launch-template-data '{\"ImageId\":\"$SERVER_V2_IMAGE_ID\",\"InstanceType\":\"t2.micro\",\"KeyName\":\"$PRIVATE_KEY\",\"SecurityGroupIds\":[\"$ASG_SG_ID\"],\"IamInstanceProfile\":{\"Name\":\"$INVENTORY_SERVER_ROLE\"}}' --query 'LaunchTemplate.LaunchTemplateId' --output text)"
+
     status=$?
 fi
 
